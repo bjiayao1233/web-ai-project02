a630ed496e703ff8ce5bae6c116807e1bd7cfafa
jy <2511335947@qq.com>
2025-07-22 15:46:53 +0800

    第二次提交

--- tlias-web-management/src/main/java/com/example/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/example/controller/DeptController.java
@@ -4,10 +4,7 @@ package com.example.controller;
 import com.example.entity.Dept;
 import com.example.entity.Result;
 import com.example.service.DeptService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
